331ef8940465a39d84a13609a6f78e974cf856c5
iammithun <157653522+iammithun@users.noreply.github.com>
2024-02-25 10:37:27 -0500

    Add files via upload

---
+++ triangle_in_a_circle.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 10:03:13 2024
+
+@author: iamrs
+"""
+
+import turtle as t
+t.bgcolor("blue")
+t.hideturtle()
+t.circle(100)
+t.forward(100)
+t.right(120)
+t.left(140)
+t.right(150)
+t.left(160)
+t.right(170)
+t.left(180)
+t.right(120)
+t.left(140)
+t.right(120)
+t.left(140)
+t.right(120)
+t.left(140)
+t.right(120)
+t.left(140)
